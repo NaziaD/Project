dcde60130eee6ef6b515e06a7d2f93b526346374
NaziaD <nazdinath@gmail.com>
2019-11-15 15:38:30 -0600

    Added Item constructor and display method.

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,14 @@
 public class Item {
+    private String itemName;
+    private int itemCode;
+
+    public Item(String name, int code){
+        itemName = name;
+        itemCode = code;
+    }
+    public void display(){
+        System.out.println("The product name is: "+itemName);
+        System.out.println("The product code is: "+itemCode);
+    }
+
 }
